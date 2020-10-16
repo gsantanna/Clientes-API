55b56eabae4ce1ea789c8fa530aed553fceaedd2
Gustavo da Cunha Santanna <gustavo@biexpert.com.br>
2020-10-16 20:49:50 -0300

    Create client entity

--- Project-Client-API/Project.Domain/Entities/Cliente.cs
+++ Project-Client-API/Project.Domain/Entities/Cliente.cs
@@ -1,11 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Project.Domain.Entities
 {
     public class Cliente
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
 
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(Guid id, string nome, string cpf, int idade)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            Idade = idade;
+        }
     }
 }
