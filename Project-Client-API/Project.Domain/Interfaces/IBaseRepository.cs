8d7494bce61128b88635b303364c3f011bfd6122
Gustavo da Cunha Santanna <gustavo@biexpert.com.br>
2020-10-16 20:57:27 -0300

    Create and config CRUD contract to Cliente entity

---
+++ Project-Client-API/Project.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Project.Domain.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Update(TEntity obj);
+        void Remove(TEntity obj);
+        IQueryable<TEntity> GetAll();
+        TEntity GetById(Guid id);
+    }
+}
---
+++ Project-Client-API/Project.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Domain.Interfaces
+{
+    public interface IClienteRepository : IBaseRepository<Cliente>
+    {
+        Cliente GetByCpf(string cpf);
+    }
+}
