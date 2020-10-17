a966d09d0b1ab7fd0f214b06646be2524a21f30e
Gustavo da Cunha Santanna <gustavo@biexpert.com.br>
2020-10-16 21:47:31 -0300

    Create Endereco and validation to Endereco Entity

---
+++ Project-Client-API/Project.Domain/Entities/Endereco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Domain.Entities
+{
+    public class Endereco
+    {
+        public Guid Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        public Endereco()
+        {
+
+        }
+
+        public Endereco(Guid id, string logradouro, string bairro, string cidade, string estado)
+        {
+            Id = id;
+            Logradouro = logradouro;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+        }
+    }
+}
---
+++ Project-Client-API/Project.Domain/Validations/EnderecoValidation.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Project.Domain.Entities;
+
+namespace Project.Domain.Validations
+{
+    public class EnderecoValidation : AbstractValidator<Endereco>
+    {
+        public EnderecoValidation()
+        {
+            RuleFor(a => a.Id)
+                .NotEmpty().WithMessage("Id do cliente obrigatório.");
+
+            RuleFor(a => a.Logradouro)
+                .NotEmpty().WithMessage("Logradouro do cliente obrigatório.")
+                .Length(1, 50).WithMessage("Nome de ter de 1 a 50 caracteres.");
+
+            RuleFor(a => a.Bairro)
+                .NotEmpty().WithMessage("Bairro do cliente obrigatório.")
+                .Length(1, 40).WithMessage("Nome de ter de 1 a 40 caracteres.");
+
+            RuleFor(a => a.Cidade)
+                .NotEmpty().WithMessage("Cidade do cliente obrigatório.")
+                .Length(1, 40).WithMessage("Nome de ter de 1 a 40 caracteres.");
+
+            RuleFor(a => a.Estado)
+                .NotEmpty().WithMessage("Estado do cliente obrigatório.")
+                .Length(1, 40).WithMessage("Nome de ter de 1 a 40 caracteres.");
+        }
+    }
+}
